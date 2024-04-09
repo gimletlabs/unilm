56d5423aeb5b9e6273c1e90aae063131e4e65053
Phillip Kuznetsov <philkuz@gimletlabs.ai>
2024-04-08 17:26:38 -0700

    drop the long description
    Signed-off-by: Phillip Kuznetsov <philkuz@gimletlabs.ai>

--- setup.py
+++ setup.py
@@ -2,13 +2,11 @@ from setuptools import find_packages, setup
 
 setup(
     name="beit3-gml",
-    version="0.1.0",
+    version="0.1.1",
     author="Gimlet Labs",
     url="https://github.com/gimletlabs/unilm",
     license="MIT",
     description="Package for beit3, part of UniLM by Microsoft, customized for GML tasks",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
     packages=find_packages(where=".", include=["beit3"]),
     install_requires=open("beit3/requirements.txt").read().splitlines(),
     python_requires=">=3.8",
