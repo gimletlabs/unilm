cb9c38b67b945e89fc6b23a39ca779d0a5f89edb
James Bartlett <james@gimletlabs.ai>
2024-04-30 16:57:14 -0700

    [GML] Make opencv headless
    Signed-off-by: James Bartlett <james@gimletlabs.ai>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="beit3-gml",
-    version="0.1.1",
+    version="0.1.2",
     author="Gimlet Labs",
     url="https://github.com/gimletlabs/unilm",
     license="MIT",
